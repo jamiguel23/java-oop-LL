76f37ef09009fbadbfe22efd6381ffd826bde8b1
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-12-21 17:36:48 -0800

    working on chapter 1 exercises

--- 01_02/end/Objects/src/Main.java
+++ 01_02/end/Objects/src/Main.java
@@ -1,9 +1,20 @@
 public class Main {
 
     public static void main(String[] args) {
-        Tree myFavoriteOakTree = new Tree(25,
+        Tree myFavoriteOakTree = new Tree(120,
                 5, TreeType.OAK);
 
-        System.out.println(myFavoriteOakTree.treeType);
+        myFavoriteOakTree.anncouceTallTree();
+
+        Tree myFavoriteMapleTree = new Tree(101,
+                30, TreeType.MAPLE);
+
+        myFavoriteMapleTree.anncouceTallTree();
+
+
+
+//        System.out.println(myFavoriteOakTree.treeType);
+
+
     }
 }
--- 01_03/begin/Objects/src/Main.java
+++ 01_03/begin/Objects/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Tree myFavoriteOakTree = new Tree(120,
                 12, TreeType.OAK);
+
     }
 }
