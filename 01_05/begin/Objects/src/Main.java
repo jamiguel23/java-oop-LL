bbc455d5196fd3627c7d251c02163e4fd97f5894
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2022-12-21 17:55:03 -0800

    01-05

--- 01_05/begin/Objects/src/Main.java
+++ 01_05/begin/Objects/src/Main.java
@@ -5,5 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         Color myTrunkColor = Tree.TRUNK_COLOR;
+        Color myDefaultWhite = Color.WHITE;
+        Color myDefaultBlue = Color.blue;
+
+
+        color brighterBlue = myDefaultBlue.brighter();
+        
     }
 }
